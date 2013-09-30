3a64b2614dbac8b9b512c063833cf74b369efe51
Thibault Miclo <thibault.miclo@utbm.fr>
2013-09-30 17:49:12 -0400

    Auto content bug fix

--- wfc_files/theme_functions/wfc_helper_functions.php
+++ wfc_files/theme_functions/wfc_helper_functions.php
@@ -135,13 +135,13 @@
                     <li>Aliquam tincidunt mauris eu risus.</li>
                 </ul>
 
-                <img class="aligncenter" src="<?php echo WFC_IMG_URI;?>/box-1-5.jpg" alt="" />
+                <img class="aligncenter" src="'.WFC_IMG_URI.'/box-1-5.jpg" alt="" />
 
                  <p>
                     <strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.
                 </p>
 
-                <img class="aligncenter" src="<?php echo WFC_IMG_URI;?>/box-1-5.jpg" alt="" />
+                <img class="aligncenter" src="'.WFC_IMG_URI.'/box-1-5.jpg" alt="" />
 
             </div><!--end #wfcContent-->
         </div><!--end #container-->';
