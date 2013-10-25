14529464f4496e971c888230b56415a9dfbb3ca5
Thibault Miclo <thibault.miclo@utbm.fr>
2013-10-25 15:50:34 -0400

    Shortcuts improvment
    Check the box by default for PDF & external link
    Effect on change

--- wfc_files/admin/js/scf.jquery.menu.manager.js
+++ wfc_files/admin/js/scf.jquery.menu.manager.js
@@ -7,59 +7,53 @@ jQuery(function ($) {
     var newBtn = '<a class="add-new-h2" href="'+href_link+'/post-new.php?post_type=page&shortcut=true">Add Shortcut</a>';
     $(newBtn).appendTo($('.add-new-h2').parent());
 
+    var s=parseInt($('#wfc_page_type_shortcut option:selected').val());
     $('#wfc_page_existing_pages').hide(600);
-    $('#wfc_page_existing_posts').hide(600);
     $('#wfc_page_external_link').hide(600);
-    $('#wfc_page_existing_images').hide(600);
     $('#wfc_page_existing_pdfs').hide(600);
+    switch(s)
+    {
+        case 1:
+            $('#wfc_page_existing_pages').show(600);
+        break;
+        case 2:
+             $('#wfc_page_external_link').show(600);
+        break;
+        case 3:
+            $('#wfc_page_existing_pdfs').show(600);
+        break;
+    }
+
     $('#wfc_page_type_shortcut').change(function () {
         var s=parseInt($('#wfc_page_type_shortcut option:selected').val());
         switch(s)
         {
             case 1:
                 $('#wfc_page_existing_pages').show(600);
-                $('#wfc_page_existing_posts').hide(600);
                 $('#wfc_page_external_link').hide(600);
-                $('#wfc_page_existing_images').hide(600);
                 $('#wfc_page_existing_pdfs').hide(600);
             break;
             case 2:
                 $('#wfc_page_existing_pages').hide(600);
-                $('#wfc_page_existing_posts').hide(600);
                 $('#wfc_page_external_link').show(600);
-                $('#wfc_page_existing_images').hide(600);
                 $('#wfc_page_existing_pdfs').hide(600);
+                $('wfc_page_new_tab_option[]').prop('checked', true);
             break;
             case 3:
                 $('#wfc_page_existing_pages').hide(600);
-                $('#wfc_page_existing_posts').hide(600);
                 $('#wfc_page_external_link').hide(600);
-                $('#wfc_page_existing_images').show(600);
-                $('#wfc_page_existing_pdfs').hide(600);
-            break;
-            case 4:
-                $('#wfc_page_existing_pages').hide(600);
-                $('#wfc_page_existing_posts').hide(600);
-                $('#wfc_page_external_link').hide(600);
-                $('#wfc_page_existing_images').hide(600);
                 $('#wfc_page_existing_pdfs').show(600);
-            break;
-            case 5:
-                $('#wfc_page_existing_pages').hide(600);
-                $('#wfc_page_existing_posts').show(600);
-                $('#wfc_page_external_link').hide(600);
-                $('#wfc_page_existing_images').hide(600);
-                $('#wfc_page_existing_pdfs').hide(600);
+                $('wfc_page_new_tab_option[]').prop('checked', true);
             break;
             default:
                 $('#wfc_page_existing_pages').hide(600);
-                $('#wfc_page_existing_posts').hide(600);
                 $('#wfc_page_external_link').hide(600);
-                $('#wfc_page_existing_images').hide(600);
                 $('#wfc_page_existing_pdfs').hide(600);
             break;
         }
+        $('#wfc_page_existing_pages').css('background-color', 'rgba(235,185,35,0)').animate({'opacity': 1}, 500);
+        $('#wfc_page_external_link').css('background-color', 'rgba(235,185,35,0)').animate({'opacity': 1}, 500);
+        $('#wfc_page_existing_pdfs').css('background-color', 'rgba(235,185,35,0)').animate({'opacity': 1}, 500);
     });
-    $('#wfc_page_type_shortcut').trigger('change');
 });
 
