4aa5eb37320acbf47218f6f00aea9abb2f9680c9
Steve Fischer <stevecfischer@gmail.com>
2013-11-18 16:56:09 -0500

    - (issue #21) reworded text. updated static input fields for templates to dynamic loop of the available templates.

--- wfc_files/theme_functions/build_theme.php
+++ wfc_files/theme_functions/build_theme.php
@@ -5,11 +5,9 @@
      * @author Steve
      * @date 7/20/13
      */
-
     /*
      * @sftodo: We should really integrate this with post types, meta boxes. Ex user enters custom post type like staff in form field and this class will build required files.  like staff-archive.php, dept-taxonomy.php, staff-single.php etc etc.
      */
-
     $themename = get_bloginfo( 'name' );
     $shortname = "wfc_";
     function Wfc_Build_Theme_Page(){
@@ -36,67 +34,68 @@
         if( file_exists( WFC_PT.'../header.php' ) ){
             die("Theme already built.  Go Fish.");
         }
-        if( isset($_REQUEST['build']) && $_REQUEST['build'] ){
-            $header       = '<!DOCTYPE html>
-                <!--
-                 __      __     _____   ______
-                /\ \  __/\ \  /\  ___\ /\  _  \
-                \ \ \/\ \ \ \ \ \ \__/ \ \ \/\_\
-                 \ \ \ \ \ \ \ \ \  __\ \ \ \/ /_
-                  \ \ \_/\ _\ \ \ \ \_/  \ \ \_\ \
-                   \ \__/ \___/  \ \_\    \ \____/
-                    \/__/ /__/    \/_/     \/___/
-                -->
-                <!--[if lt IE 7 ]>
-                <html class="ie ie6" <?php language_attributes(); ?>>
-                <![endif]-->
-                <!--[if IE 7 ]>
-                <html class="ie ie7" <?php language_attributes(); ?>>
-                <![endif]-->
-                <!--[if IE 8 ]>
-                <html class="ie ie8" <?php language_attributes(); ?>>
-                <![endif]-->
-                <!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes(); ?>> <!--<![endif]-->
-                <head>
-                    <meta charset="<?php bloginfo( "charset" ); ?>"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title><?php bloginfo("name"); ?> | <?php is_front_page() ? bloginfo("description") : wp_title(""); ?></title>
-                    <link rel="profile" href="http://gmpg.org/xfn/11"/>
-                    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( "stylesheet_url" ); ?>"/>
-                    <link rel="pingback" href="<?php bloginfo( "pingback_url" ); ?>"/>
-                    <?php wp_head(); ?>
-                </head>
-                <body <?php body_class(); ?>>';
-            $footer       = '<!-- FOOTER -->
-                <footer>
-                    <p class="pull-right">
-                        <a href="#">Back to top</a>
-                    </p>
-                    <div id="wfc-footer-links">
-                        <p>
-                            <a target="_blank" href="http://www.webfullcircle.com">Internet Marketing</a>
-                            |
-                            <a target="_blank" href="http://www.webfullcircle.com">Website Design</a>
-                        </p>
-                    </div>
-                </footer>
-                </div>
-                <!-- /.container -->
-                <!-- Bootstrap core JavaScript
-                ================================================== -->
-                <!-- Placed at the end of the document so the pages load faster -->
-                <?php wp_footer(); ?>
-                </body>
-                </html';
-            $page         = '
+        $header       = '
+<!DOCTYPE html>
+<!--
+ __      __     _____   ______
+/\ \  __/\ \  /\  ___\ /\  _  \
+\ \ \/\ \ \ \ \ \ \__/ \ \ \/\_\
+ \ \ \ \ \ \ \ \ \  __\ \ \ \/ /_
+  \ \ \_/\ _\ \ \ \ \_/  \ \ \_\ \
+   \ \__/ \___/  \ \_\    \ \____/
+    \/__/ /__/    \/_/     \/___/
+-->
+<!--[if lt IE 7 ]>
+<html class="ie ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7 ]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8 ]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <meta charset="<?php bloginfo( "charset" ); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo("name"); ?> | <?php is_front_page() ? bloginfo("description") : wp_title(""); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11"/>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( "stylesheet_url" ); ?>"/>
+    <link rel="pingback" href="<?php bloginfo( "pingback_url" ); ?>"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>';
+        $footer       = '
+<footer>
+    <p class="pull-right">
+        <a href="#">Back to top</a>
+    </p>
+    <div id="wfc-footer-links">
+        <p>
+            <a target="_blank" href="http://www.webfullcircle.com">Internet Marketing</a>
+            |
+            <a target="_blank" href="http://www.webfullcircle.com">Website Design</a>
+        </p>
+    </div>
+</footer>
+</div>
+<!-- /.container -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<?php wp_footer(); ?>
+<?php wfc_footer(); ?>
+</body>
+</html';
+        $page         = '
                 <?php get_header(); ?>
                 <?php Wfc_Core_Page_Loop(); ?>
                 <?php get_footer(); ?>';
-            $frontpage    = '
+        $frontpage    = '
                 <?php get_header(); ?>
                 <?php Wfc_Core_Home_Page_Loop(); ?>
                 <?php get_footer(); ?>';
-            $search       = '
+        $search       = '
                 <?php get_header(); ?>
                 if( have_posts() ) :
                     while( have_posts() ) : the_post();
@@ -107,22 +106,23 @@
                 endif;
                 wp_reset_query();
                 <?php get_footer(); ?>';
-            $four_o_four  = 'echo "404 error."';
-            $editor_style = "";
-            $single       = "";
-            $archive      = "";
-            $theme_array  =
-                array(
-                    array('file' => 'header.php', 'content' => $header),
-                    array('file' => 'footer.php', 'content' => $footer),
-                    array('file' => 'page.php', 'content' => $page),
-                    array('file' => 'search.php', 'content' => $search),
-                    array('file' => '404.php', 'content' => $four_o_four),
-                    array('file' => 'editor-style.css', 'content' => $editor_style),
-                    array('file' => 'single.php', 'content' => $single),
-                    array('file' => 'front-page.php', 'content' => $frontpage),
-                    array('file' => 'archive.php', 'content' => $archive)
-                );
+        $four_o_four  = 'echo "404 error."';
+        $editor_style = "";
+        $single       = "";
+        $archive      = "";
+        $theme_array  =
+            array(
+                array('file' => 'header.php', 'content' => $header),
+                array('file' => 'footer.php', 'content' => $footer),
+                array('file' => 'page.php', 'content' => $page),
+                array('file' => 'search.php', 'content' => $search),
+                array('file' => '404.php', 'content' => $four_o_four),
+                array('file' => 'editor-style.css', 'content' => $editor_style),
+                array('file' => 'single.php', 'content' => $single),
+                array('file' => 'front-page.php', 'content' => $frontpage),
+                array('file' => 'archive.php', 'content' => $archive)
+            );
+        if( isset($_REQUEST['build']) && $_REQUEST['build'] ){
             //Replace _ by . in Request variable
             $keys                   = implode( ',', array_keys( $_REQUEST ) );
             $keys                   = str_replace( '_', '.', $keys );
@@ -143,19 +143,22 @@
 
             <form method="post">
                 <p class="choices">
-                The following files will be created automatically:<br />
-                    Header.php <br />
-                    Footer.php <br />
+                    The following files will be created automatically:<br/>
+                    Header.php <br/>
+                    Footer.php <br/>
                     <br/>
-                <!-- Required since we check if header.php exists to know if we need to build out the theme -->
-                You can choose to build the following files or not:<br />
-                    Archive.php : <input type="checkbox" name="archive.php"/><br/>
-                    Page.php : <input type="checkbox" name="page.php"/><br/>
-                    Front-page.php : <input type="checkbox" name="front-page.php"/><br/>
-                    Search.php : <input type="checkbox" name="search.php"/><br/>
-                    404.php : <input type="checkbox" name="404.php"/><br/>
-                    Single.php : <input type="checkbox" name="single.php"/><br/>
-                    Editor-Style.css : <input type="checkbox" name="editor-style.css"/><br/>
+                    <!-- Required since we check if header.php exists to know if we need to build out the theme -->
+                    You can choose to build the following files or not:<br/>
+                    <?php
+                        foreach( $theme_array as $template ){
+                            if( $template['file'] != "header.php" && $template['file'] != "footer.php" ){
+                                ?>
+                                <input type="checkbox" name="<?php echo $template['file']; ?>"/> <?php echo $template['file']; ?>
+                                <br/>
+                            <?php
+                            }
+                        }
+                    ?>
                 </p>
                 <p class="submit">
                     <input name="build" type="submit" value="Build Out Theme"/>
