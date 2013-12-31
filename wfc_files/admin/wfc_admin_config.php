2bdc218ad0614475f368d9a91add0912e4c9f91a
Steve Fischer <stevecfischer@gmail.com>
2013-12-31 06:36:45 -0500

    - moved global_config.php into admin folder; seems silly to have folder for one file.

--- wfc_files/admin/wfc_admin_config.php
+++ wfc_files/admin/wfc_admin_config.php
@@ -14,7 +14,7 @@
      */
     require_once(WFC_CONFIG.'/wfc_developer_login.php'); //Auto login inside WFC IP Address
     require_once(WFC_ADM.'/wfc_post_type_manager.php'); //CPT / Tax / Metabox Class
-    require_once(WFC_GLOBAL.'/wfc_global_config.php'); //Global hooks/functions
+    require_once(WFC_ADM.'/wfc_global_config.php'); //Global hooks/functions
     require_once(WFC_CONFIG.'/wfc_security.php'); //Setup Framework Security
     require_once(WFC_ADM.'/wfc_expanded_menu_manager.php'); //CPT / Tax / Metabox Class
     require_once(WFC_ADM.'/wfc_browser_check.php'); //Alerts Old Browsers
